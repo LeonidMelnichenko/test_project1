5a742269d33ac5aeb5c56ed4bbd563af4f3ca0c7
DikunIG <ilia.dikun.extern@elinvar.de>
2023-01-22 14:38:31 +0300

    error added

--- src/DayCounter.java
+++ src/DayCounter.java
@@ -51,7 +51,7 @@ public class DayCounter {
                 newYear = year;
             }
         } else {
-            newDay = day + 1;
+            newDay = day + 2;
             newMonth = month;
             newYear = year;
         }
