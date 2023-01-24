579a5cd5b2e8c7bf8c3d3041b8722411c7c3ee58
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-01-24 23:11:47 +0300

    Task2

---
+++ src/homework1/Task2.java
@@ -0,0 +1,15 @@
+package homework1;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int[] array = new int[]{0, 1, 1, 2, 3, 5, 8, 13, 21};
+        int sumChet = 0;
+
+        for(int i = 0; i < array[6]; ++i) {
+            if (array[i] % 2 == 0) {
+                sumChet += array[i];
+            }
+        }
+        System.out.println("Сумма всех четных чисел до 7 индекса равна: " + sumChet);
+    }
+}
