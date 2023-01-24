172b58a3c4be7a2f4dbc59ace91d318493a0ea61
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-01-24 23:13:27 +0300

    Task3

---
+++ src/homework1/Task3.java
@@ -0,0 +1,20 @@
+package homework1;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int[] array = new int[]{0, 1, 1, 2, 3, 5, 8, 13, 21};
+        int n = array.length - 1;
+
+        int i;
+        for(i = 0; i < array.length / 2; ++i) {
+            int buf = array[i];
+            array[i] = array[n];
+            array[n] = buf;
+            --n;
+        }
+        System.out.print("массив после инверсии будет иметь следующий вид: ");
+        for(i = 0; i < array.length; ++i) {
+            System.out.print(array[i] + ", ");
+        }
+    }
+}
