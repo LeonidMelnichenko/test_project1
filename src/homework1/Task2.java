42eebdf84d2e6d3904dedb97152b3e23c00097ec
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-01-29 12:14:16 +0300

    Task2

--- src/homework1/Task2.java
+++ src/homework1/Task2.java
@@ -7,8 +7,6 @@ public class Task2 {
         array[1] = 1;
         for (int i = 2; i < array.length; i++) {
             array[i] = array[i - 1] + array[i - 2];
-        }
-        for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
         System.out.println();
