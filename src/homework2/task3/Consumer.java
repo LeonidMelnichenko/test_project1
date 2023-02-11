d42d7b080a31c9febada924e663b5816997da915
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-02-11 14:12:34 +0300

    Задача № 3 (с перечислениями) на проверку

--- src/homework2/task3/Consumer.java
+++ src/homework2/task3/Consumer.java
@@ -1,33 +1,33 @@
 package src.homework2.task3;
-    public class Consumer {
-        private final int[] lastNumbersArray;
-        public Consumer(int arraysize) {
-            this.lastNumbersArray = new int[arraysize];
-        }
-        private int counter = 0;
-        private int iteraition = 0;
+public class Consumer {
+    private final int[] lastNumbersArray;
 
-        public void consume(int lastNumber) {
-            lastNumbersArray[counter] = lastNumber;
-                counter++;
-                iteraition++;
-            if (counter == lastNumbersArray.length) {
-                counter = 0;
+    public Consumer(int arraysize) {
+        this.lastNumbersArray = new int[arraysize];
+    }
+
+    private int counter = 0;
+
+    public void consume(int lastNumber) {
+        lastNumbersArray[counter] = lastNumber;
+        counter++;
+        if (counter == lastNumbersArray.length) {
+            counter = 0;
+        }
+    }
+    public double avg() {
+        double sum = 0;
+        for (int i = 0; i < lastNumbersArray.length; i++) {
+            sum += lastNumbersArray[i];
+            if (lastNumbersArray[i] < 5) {
+                System.out.println(sum / lastNumbersArray[i]);
+            } else {
+                System.out.println(sum / 5);
             }
         }
-        public double avg() {
-            double sum = 0;
-                for (int i = 0; i < lastNumbersArray.length; i++) {
-                    sum += lastNumbersArray[i];
-                }
-                if(iteraition < 5){
-                    System.out.println(sum/iteraition);
-                } else {
-                    System.out.println(sum/5);
-                }
-                return 0;
-            }
-        }
+        return 0;
+    }
+}
 
 
 
--- src/homework2/task3/MainConsumer.java
+++ src/homework2/task3/MainConsumer.java
@@ -4,14 +4,14 @@ public class MainConsumer {
     public static void main(String[] args) {
         Consumer consumer = new Consumer(5);
         consumer.consume(1);
-        consumer.consume(2);
-        consumer.consume(3);
-//        consumer.consume(4);
-//        consumer.consume(5);
-//        consumer.consume(6);
-//        consumer.consume(7);
-//        consumer.consume(8);
-//        consumer.consume(9);
+        consumer.consume(1);
+        consumer.consume(1);
+        consumer.consume(1);
+        consumer.consume(1);
+        consumer.consume(1);
+        consumer.consume(1);
+        consumer.consume(1);
+        consumer.consume(1);
         double avg = consumer.avg();
        }
     }
