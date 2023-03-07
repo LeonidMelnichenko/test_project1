3f1cf548914851655f21f0c4e75c5feb8f8eec0a
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-03-07 20:16:18 +0300

    Задача № 1 домашка № 5 на проверку

---
+++ src/homework5/Task1.java
@@ -0,0 +1,14 @@
+package src.homework5;
+//Необходимо реализовать программу, которая будет бесконечно
+// считывать строки введенные пользователем и выводить их в консоль.
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter words:");
+        while (true){
+            in.next();
+        }
+    }
+}
