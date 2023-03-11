ffd778f39f817b025e1c276485122c1af6546118
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-03-11 18:50:07 +0300

    Задача № 3 домашка № 5

--- src/homework5/Task3.java
+++ src/homework5/Task3.java
@@ -11,21 +11,29 @@ public class Task3 {
     public static void main(String[] args) {
         ArrayList<Integer> nums = new ArrayList<Integer>();
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter number");
+        int a;
+        String b;
+        System.out.println("You entered integer :");
         while (true) {
             if (in.hasNextInt()) {
-                nums.add(in.nextInt());
-            } else {
-                in.equals("stop");
-                in.equals("status");
-                Collections.sort(nums);
-                System.out.println("sorted array" + nums);
-                break;
+                a = in.nextInt();
+                    nums.add(a);
+                        System.out.println("You entered integer :");
+                }
+            if (in.hasNextLine()) {
+                if (in.nextLine().equals("stop")) {
+                    b = in.nextLine();
+                        if (b.equals("status")) {
+                            Collections.sort(nums);
+                                System.out.println("sorted array" + nums);
+                                    break;
+                                }
+                            }
+                        }
+                    }
                 }
             }
-        in.close();
-        }
-    }
+
 
 
 
